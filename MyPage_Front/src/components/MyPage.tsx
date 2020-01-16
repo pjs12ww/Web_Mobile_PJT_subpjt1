c006534ee30ae140fb9ef388b8968d9ef852a188
JSP <pjs56ww@gmail.com>
2020-01-16 17:46:20 +0900

    S02P11A313-5 S02P11A313-12 MyPage friendslist 기초 코드 업데이트

--- MyPage_Front/src/components/MyPage.tsx
+++ MyPage_Front/src/components/MyPage.tsx
@@ -8,6 +8,8 @@ import Header from './Header';
 import MyPageHead from './MyPageHead';
 import MyPageProfile from './MyPageProfile';
 import MyPageReview from './MyPageReview';
+import MyPageFrinedsList from './MyPageFriendsList';
+import MyPageFriendsList from './MyPageFriendsList';
 
 const sections = [
     { title: '정보', url: '#' },
@@ -20,32 +22,56 @@ const profile = [
     { title: 'Profile' },
     { title: 'name' },
     { title: 'location' },
-    { title: 'prefer_wine_list'}
+    { title: 'prefer_wine_list' }
 ]
 
 // 임시 Review data
 const reviews = [
-    { profileImg: '',
-    name: '카카',
-    content: '적셔',
+    {
+        profileImg: '',
+        name: '카카',
+        content: '적셔',
 
-},
-{ profileImg: '',
-    name: '카카',
-    content: '적셔',
+    },
+    {
+        profileImg: '',
+        name: '카카',
+        content: '적셔',
 
-},
-{ profileImg: '',
-    name: '카카',
-    content: '적셔',
+    },
+    {
+        profileImg: '',
+        name: '카카',
+        content: '적셔',
 
-}
+    }
 ]
+
+// 임시 친구목록
+const friends = [
+
+    {
+        name: '김병인',
+    },
+    {
+        name: '김주홍',
+    },
+    {
+        name: '박재성',
+    },
+    {
+        name: '최예슬',
+    },
+    {
+        name: '허대원',
+    },
+]
+
 
 const mainFeaturedPost = {
     title: 'Title of a longer featured blog post',
     description:
-      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+        "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
     image: 'https://source.unsplash.com/random',
     imgText: 'main image description',
     linkText: 'Continue reading…',
@@ -59,9 +85,10 @@ export default function MyPage() {
                 <Header title="MyReview" sections={sections} />
                 <MyPageHead post={mainFeaturedPost} />
                 <main>
-                    <Grid container spacing={10}>
-                        <MyPageProfile sections={profile}/>
-                        <MyPageReview reviews={reviews}/>
+                    <Grid container spacing={10} justify="space-between">
+                        <MyPageProfile grid-column="1/5" sections={profile} />
+                        <MyPageReview grid-column="1/2" reviews={reviews} />
+                        <MyPageFriendsList grid-column="1/5" friends={friends} />
                     </Grid>
                 </main>
             </Container>
---
+++ MyPage_Front/src/components/MyPageFriendsList.tsx
@@ -0,0 +1,45 @@
+// MyPageProfile
+// Main 내 Grid 중 오른쪽에 위치
+// 해상도가 줄어들면 없어지도록 설정 필요
+// 가입한 클럽 위에 오도록 설정? => 기능 추가?
+// 친구목록
+
+
+import * as React from 'react';
+// 타입 확인하는 라이브러리
+import * as PropTypes from 'prop-types';
+
+import { makeStyles } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles(theme => ({
+    friend: {
+        padding: theme.spacing(1),
+    },
+}));
+
+export default function MyPageFriendsList(props) {
+    const classes = useStyles(props);
+    const { friends } = props;
+    return (
+        <React.Fragment>
+            <div>
+                {
+                    friends.map(section => (
+                        <div
+                            color="inherit"
+                            key={section.title}
+                            className={classes.friend}
+                        >
+                            {section.name}
+                        </div>
+                    ))}
+            </div>
+        </React.Fragment>
+    );
+}
+
+MyPageFriendsList.propTypes = {
+    friends: PropTypes.array,
+};
